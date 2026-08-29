1362751e50e27046d6a6f6bd3d1ff6d1b148bfb4
Mahi <mahimridha478@gmail.com>
2026-08-29 11:35:49 +0600

    Add basic calculator error handling

--- src/main.py
+++ src/main.py
@@ -4,7 +4,11 @@ from utils import add, subtract, multiply, divide
 print("Name: Mahi")
 print("Today's Date:", date.today())
 
-print("Addition:", add(10, 5))
-print("Subtraction:", subtract(10, 5))
-print("Multiplication:", multiply(10, 5))
-print("Division:", divide(10, 5))
+try:
+    print("Addition:", add(10, 5))
+    print("Subtraction:", subtract(10, 5))
+    print("Multiplication:", multiply(10, 5))
+    print("Division:", divide(10, 5))
+
+except Exception as error:
+    print("An error occurred:", error)

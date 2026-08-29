de50edb86a13da504eeea9bbe94f3b500b480511
Mahi <mahimridha478@gmail.com>
2026-08-29 11:30:37 +0600

    Add division function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from utils import add, subtract, multiply
+from utils import add, subtract, multiply, divide
 
 print("Name: Mahi")
 print("Today's Date:", date.today())
@@ -7,3 +7,4 @@ print("Today's Date:", date.today())
 print("Addition:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
 print("Multiplication:", multiply(10, 5))
+print("Division:", divide(10, 5))
